ead1579cdddf15533f546378f6a9c73fc671b2c6
Kaushik Sinha <kaushiksinha1@gmail.com>
2023-02-26 11:28:33 -0500

    document

--- neev/engine.py
+++ neev/engine.py
@@ -11,15 +11,18 @@ import matplotlib.pyplot as plt
 # %% ../nbs/00_engine.ipynb 21
 class Value:
     '''stores a single scalar value and its gradient'''
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, 
+                 data,
+                 _children=(),# The children of this value
+                 _op=''):# The operation that created this value
+        self.data, self._prev, self._op = data, set(_children), _op
         
     def __add__(self, other):
-        out = Value(self.data + other.data)
+        out = Value(self.data + other.data, (self,other), '+')
         return out
     
     def __mul__(self, other):
-        out = Value(self.data * other.data)
+        out = Value(self.data * other.data, (self,other),'*')
         return out
     
     def __repr__(self):

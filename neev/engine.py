952fdce21e5e96c83df3168e500b7bc27ad06e5f
Kaushik Sinha <kaushiksinha1@gmail.com>
2023-03-06 08:04:22 -0500

    cannot visualize in engine there is a circular dependency. reworking to get fixed for now.

--- neev/engine.py
+++ neev/engine.py
@@ -8,8 +8,9 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-# %% ../nbs/00_engine.ipynb 38
-# Add ability to chain the local gradient to the incoming gradient
+# %% ../nbs/00_engine.ipynb 47
+# accumulate gradients
+# allow adding and multiplying constants
 class Value:
     '''stores a single scalar value and its gradient'''
     def __init__(self, 
@@ -23,24 +24,32 @@ class Value:
         self._backward = lambda: None #function to chain the incoming gradient with the local gradient
         
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self,other), '+')
         
         def _backward():
-            self.grad = 1.0 * out.grad
-            other.grad = 1.0 * out.grad
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
         out._backward = _backward
         
         return out
     
+    def __radd__(self, other): #if Python cannot do other + self
+        return self + other
+    
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self,other),'*')
                 
         def _backward():
-            self.grad = other.data * out.grad
-            other.grad = self.data * out.grad
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
         out._backward = _backward
         
         return out
+    
+    def __rmul__(self, other): #if Python cannot do other * self
+        return self * other
     
     def tanh(self):
         x = self.data
@@ -49,10 +58,26 @@ class Value:
         
                 
         def _backward():
-            self.grad = (1.0 - t**2) * out.grad
+            self.grad += (1.0 - t**2) * out.grad
         out._backward = _backward
         
         return out
+    
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v) 
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        
+        self.grad = 1.0
+        for node in reversed(topo): 
+            node._backward()
+        
     
     def __repr__(self):
         return f'Value(data={self.data})'

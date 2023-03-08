fa0c03a53bc31616430992b09ea27a8f186a49d5
Kaushik Sinha <kaushiksinha1@gmail.com>
2023-03-08 08:12:35 -0500

    Next step, training loop

--- neev/engine.py
+++ neev/engine.py
@@ -8,7 +8,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-# %% ../nbs/00_engine.ipynb 48
+# %% ../nbs/00_engine.ipynb 50
 # accumulate gradients
 # allow adding and multiplying constants
 # exponentiate, power, divide, subtract

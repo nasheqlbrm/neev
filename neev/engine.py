10450c90531d7e560f386e15f11fdd44a0677f3b
Kaushik Sinha <kaushiksinha1@gmail.com>
2023-03-13 08:29:55 -0400

    add dunder rtruediv

--- neev/engine.py
+++ neev/engine.py
@@ -36,9 +36,6 @@ class Value:
         
         return out
     
-    def __radd__(self, other): #if Python cannot do other + self
-        return self + other
-    
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self,other),'*')
@@ -59,41 +56,7 @@ class Value:
             self.grad += (other * ( self.data ** (other-1) )) * out.grad
         out._backward = _backward
                 
-        return out
-            
-    def __rmul__(self, other): #if Python cannot do other times self
-        return self * other
-    
-    def __truediv__(self, other): #self/other
-        return self * (other**-1)
-    
-    def __neg__(self): #-self
-        return self * -1
-    
-    def __sub__(self, other):
-        return self + (-other)
-    
-    def tanh(self):
-        x = self.data
-        t = (math.exp(2*x)-1.)/(math.exp(2*x)+1.)
-        out = Value(t,(self,),'tanh',)
-              
-        def _backward():
-            self.grad += (1.0 - t**2) * out.grad
-        out._backward = _backward
-        
-        return out
-    
-    def exp(self):
-        x = self.data
-        t = math.exp(x)
-        out = Value(t,(self,),'exp',)
-        
-        def _backward():
-            self.grad += t * out.grad
-        out._backward = _backward
-        
-        return out
+        return out    
     
     def relu(self):
         x = self.data
@@ -120,7 +83,49 @@ class Value:
         self.grad = 1.0
         for node in reversed(topo): 
             node._backward()
+            
+    def __neg__(self): #-self
+        return self * -1
+    
+    def __radd__(self, other): #if cannot do other + self
+        return self + other
+    
+    def __sub__(self, other):
+        return self + (-other)   
+    
+    def  __rsub__(self, other): #if cannot do other - self
+        return other + (-self)
+            
+    def __rmul__(self, other): #if cannot do other times self
+        return self*other
+    
+    def __truediv__(self, other): #self/other
+        return self * (other**-1)
+    
+    def __rtruediv__(self, other): # if cannot do other / self
+        return other * self**-1
+    
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x)-1.)/(math.exp(2*x)+1.)
+        out = Value(t,(self,),'tanh',)
+              
+        def _backward():
+            self.grad += (1.0 - t**2) * out.grad
+        out._backward = _backward
         
+        return out
+    
+    def exp(self):
+        x = self.data
+        t = math.exp(x)
+        out = Value(t,(self,),'exp',)
+        
+        def _backward():
+            self.grad += t * out.grad
+        out._backward = _backward
+        
+        return out
     
     def __repr__(self):
         return f'Value(data={self.data})'
